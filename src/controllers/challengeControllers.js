5b835995560b204471190fa21b4887cfd144067a
Vibilan <vibilan2793@gmail.com>
2026-04-11 14:44:03 +0530

    postchallenges endpoint created

--- src/controllers/challengeControllers.js
+++ src/controllers/challengeControllers.js
@@ -7,7 +7,20 @@ const getAllChallenges = async (req, res) => {
     res.send(data);
 }
 
-const postChallenge = (req, res) => {
+const postChallenge = async (req, res) => {
+
+    const { title, description, is_public, deadline } = req.body;
+
+    const { data, error } = await client
+        .from('challenges')
+        .insert({
+            title: title,
+            description: description,
+            creator_id: "40abf6df-0204-47bc-a50e-d0d34e1eb3bf",
+            is_public: is_public,
+            deadline: deadline
+        })
+        .select();
     res.send({ message: "Challenge Created." });
 }
 
